25c6ff2f9938ae69fcb7a1036601fbeb6fb346b7
Walter <eschenbach.walter@gmail.com>
2021-08-12 11:36:05 -0500

    updated link styling

--- components/Homepage.js
+++ components/Homepage.js
@@ -8,7 +8,7 @@ function Homepage() {
             <div className={styles.homepageContainer}>
                 <div className={styles.homepageLeft}>
                     <div className={styles.homepageText}>
-                        <p>NERF PERKS HAVE ARRIVED</p>
+                        <p style={{ fontWeight: "700" }}>NERF PERKS HAVE ARRIVED</p>
                         <h1>BUY NERF. <br />EARN NERF <br />PERKS POINTS.</h1>
                         <p>Sign up for Nerf Perks to earn rewards and a chance to win <br />tickets to the College Football Playoff National Championship.</p>
 
