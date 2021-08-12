a9bf84e144ae89adb67864a6b61fc17452a6f40e
Walter <eschenbach.walter@gmail.com>
2021-08-12 10:55:35 -0500

    additional styling & updated nav links & updated social links

--- components/Homepage.js
+++ components/Homepage.js
@@ -4,27 +4,30 @@ import Image from 'next/image'
 
 function Homepage() {
     return (
-        <div className={styles.homepageContainer}>
-            <div className={styles.homepageLeft}>
-                <div className={styles.homepageText}>
-                    <p>NERF PERKS HAVE ARRIVED</p>
-                    <h1>BUY NERF. <br />EARN NERF <br />PERKS POINTS.</h1>
-                    <p>Sign up for Nerf Perks to earn rewards and a chance to win <br />tickets to the College Football Playoff National Championship.</p>
+        <div className={styles.homepage}>
+            <div className={styles.homepageContainer}>
+                <div className={styles.homepageLeft}>
+                    <div className={styles.homepageText}>
+                        <p>NERF PERKS HAVE ARRIVED</p>
+                        <h1>BUY NERF. <br />EARN NERF <br />PERKS POINTS.</h1>
+                        <p>Sign up for Nerf Perks to earn rewards and a chance to win <br />tickets to the College Football Playoff National Championship.</p>
 
+                    </div>
+                </div>
+                <div className={styles.homepageRight}>
+
+                    <Image
+                        src="/NERF-LOGO5.png"
+                        alt=""
+                        //className={styles.circleBrand}
+                        width={500}
+                        height={500}
+                    //layout="responsive" 
+                    />
                 </div>
             </div>
-            <div className={styles.homepageRight}>
-
-                <Image
-                    src="/NERF-LOGO5.png"
-                    alt=""
-                    //className={styles.circleBrand}
-                    width={500}
-                    height={500}
-                //layout="responsive" 
-                />
-            </div>
         </div>
+
     )
 }
 
--- components/Nav.js
+++ components/Nav.js
@@ -5,8 +5,8 @@ import Image from 'next/image'
 function Nav() {
     return (
         <nav className={styles.nav}>
-            <div className={styles.navlink}><h3>PRODUCTS</h3></div>
-            <div className={styles.navlink}><h3>MERCH</h3></div>
+            <div className={styles.navlink}><a href="https://nerf.hasbro.com/en-us/toys-games"><h3>PRODUCTS</h3></a></div>
+            <div className={styles.navlink}><a href="https://www.zazzle.com/store/nerf/products"><h3>MERCH</h3></a></div>
             <div style={{ display: "block", position: "relative", width: "14rem", margin: "5px" }}>
                 <Image
                     src="/NERF-LOGO2.png"
@@ -16,8 +16,8 @@ function Nav() {
                     className={styles.brand}
                 />
             </div>
-            <div className={styles.navlink}><h3>REWARDS</h3></div>
-            <div className={styles.navlink}><h3>FAQ</h3></div>
+            <div className={styles.navlink}><a href="https://www.kohls.com/media/digital/ecom/pdfs/pdf/Hasbro_NerfPerks.pdf"><h3>REWARDS</h3></a></div>
+            <div className={styles.navlink}><a href="https://nerf.hasbro.com/en-us/instructions"><h3>FAQ</h3></a></div>
         </nav>
     )
 }
